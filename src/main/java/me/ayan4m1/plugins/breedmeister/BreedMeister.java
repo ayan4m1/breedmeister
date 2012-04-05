40dce55e453adcb0a184af7db9e8a7abd3749c7b
ayan4m1 <andrew@bulletlogic.com>
2012-04-05 15:48:59 -0400

    two animals now required to breed, implemented vanilla-style breeding delay

--- src/main/java/me/ayan4m1/plugins/breedmeister/BreedMeister.java
+++ src/main/java/me/ayan4m1/plugins/breedmeister/BreedMeister.java
@@ -1,8 +1,12 @@
 package me.ayan4m1.plugins.breedmeister;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Animals;
 import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
@@ -11,35 +15,88 @@ import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BreedMeister extends JavaPlugin implements Listener {
-    public void onDisable() {
-        //TODO: No disable code needed for now
-    }
+	//Map of entity ids to timestamps representing when they may next breed
+	private HashMap<Integer, Long> breedTimes = new HashMap<Integer, Long>();
 
-    public void onEnable() {
-        getServer().getPluginManager().registerEvents(this, this);
-    }
-    
-    @EventHandler
-    public void onBlockDispense(BlockDispenseEvent event) {
-    	if (event.getBlock().getType() == Material.DISPENSER) {
-    		if (event.getItem().getType() == Material.WHEAT) {
-    			List<Entity> entities = event.getBlock().getWorld().getEntities();
-    			for (Entity e : entities) {
-    				if (!(e instanceof Animals && ((Animals)e).canBreed()) || e.isDead()) {
-    	    			continue;
-    	    		}
+	//Maximum distance from dispenser to animal in blocks
+	private Integer maxDistance = 5;
 
-    	    		if (event.getBlock().getLocation().distance(e.getLocation()) > 5) {
-    	    			continue;
-    	    		}
+	//Per-animal delay between breeding in minutes
+	private Integer breedDelay = 5;
 
-    	    		Animals newAnimal = (Animals)event.getBlock().getWorld().spawnCreature(e.getLocation().add(1, 0, 1), e.getType());
-    	    		newAnimal.setBaby();
-    	    		event.setCancelled(true);
-    	    		break;
-    			}
-    		}
-    	}
-    }
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	public void onDisable() {
+		this.breedTimes.clear();
+	}
+
+	@EventHandler
+	public void onBlockDispense(BlockDispenseEvent event) {
+		Block block = event.getBlock();
+
+		//Only continue if wheat is being dropped from a dispenser
+		if (block.getType() != Material.DISPENSER || event.getItem().getType() != Material.WHEAT) {
+			return;
+		}
+
+		List<Entity> entities = block.getWorld().getEntities();
+
+		//Search for two valid and distinct animals
+		Animals animalOne = this.findValidAnimal(entities, block.getLocation(), null);
+		if (animalOne == null) {
+			this.getLogger().info("Found no animals");
+			return;
+		}
+
+		Animals animalTwo = this.findValidAnimal(entities, block.getLocation(), animalOne);
+		if (animalTwo == null) {
+			this.getLogger().info("Found only one animal");
+			return;
+		}
+
+		//Spawn a baby and cancel the BlockDispenseEvent
+		Animals newAnimal = (Animals)block.getWorld().spawnCreature(animalTwo.getLocation().add(1, 0, 1), animalTwo.getType());
+		newAnimal.setBaby();
+		event.setCancelled(true);
+
+		//Add the animals to the bred list
+		Long nextBreedTime = (new Date().getTime() / 1000) + (this.breedDelay * 60);
+		this.breedTimes.put(animalOne.getEntityId(), nextBreedTime);
+		this.breedTimes.put(animalTwo.getEntityId(), nextBreedTime);
+	}
+
+	private Animals findValidAnimal(List<Entity> entities, Location dispenserLoc, Animals exceptThis) {
+		for(Entity e : entities) {
+			//Ensure entity is a living, adult animal
+			if (!(e instanceof Animals && ((Animals)e).canBreed()) || e.isDead()) {
+				continue;
+			}
+
+			//Ensure animal is within maxDistance blocks from the dispenser
+			if (dispenserLoc.distance(e.getLocation()) > this.maxDistance) {
+				continue;
+			}
+
+			//Ensure the animal can breed
+			if (this.breedTimes.containsKey(e.getEntityId())) {
+				if (this.breedTimes.get(e.getEntityId()) > (new Date().getTime() / 1000)) {
+					continue;
+				} else {
+					this.breedTimes.remove(e.getEntityId());
+				}
+			}
+
+			//Ensure the animal is not one we have already found
+			if (exceptThis != null
+					&& e.getType().equals(exceptThis.getType())
+					&& exceptThis.getEntityId() != e.getEntityId()) {
+				continue;
+			}
+
+			return (Animals)e;
+		}
+		return null;
+	}
 }
-
